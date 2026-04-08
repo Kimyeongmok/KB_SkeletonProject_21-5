a10936fea1a064da4893d8d3802b84ba675c0020
hjimin027 <hjimin027@gmail.com>
2026-04-08 17:10:12 +0900

    fix: fix error caused by refactor

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
-import LoginView from "@/views/LoginVue.vue";
+import LoginView from "@/views/LoginView.vue";
 import { useAuthStore } from "@/stores/auth";
 
 const router = createRouter({
