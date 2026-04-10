81205b891636d0d8f7ca59cf35c15fccb0234e39
ksj <rhtnwo0906@gmail.com>
2026-04-10 17:32:07 +0900

    style: BudgetView에 CurrentBudget 추가

--- src/views/BudgetView.vue
+++ src/views/BudgetView.vue
@@ -1,10 +1,14 @@
 <script setup>
+import CurrentBudget from '@/components/CurrentBudget.vue';
 import BudgetCard from '@/components/BudgetCard.vue';
 import RecentTransaction from '@/components/RecentTransaction.vue';
 </script>
 
 <template>
     <div class ="flex flex-col w-full gap-4">
+        <div class ="w-full">
+            <CurrentBudget/>
+        </div>
         <div class ="w-full">
             <BudgetCard/>
         </div>
