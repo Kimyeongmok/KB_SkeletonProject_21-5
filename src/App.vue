f39db31f29a44b3099bebabb70ccc417a4fbb323
ksj <rhtnwo0906@gmail.com>
2026-04-10 13:05:35 +0900

    style: App.vue 수정

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { computed } from "vue";
-import { RouterView, useRoute } from "vue-router";
-import Header from "@/components/Header.vue";
-import Menu from "@/components/Menu.vue";
-import UserBalance from "@/components/UserBalance.vue";
+import { computed } from 'vue';
+import { RouterView, useRoute } from 'vue-router';
+import Header from '@/components/Header.vue';
+import Menu from '@/components/Menu.vue';
+import UserBalance from '@/components/UserBalance.vue';
 
 const route = useRoute();
-const isAuthLayout = computed(() => route.meta.layout === "auth");
+const isAuthLayout = computed(() => route.meta.layout === 'auth');
 </script>
 
 <template>
@@ -18,19 +18,23 @@ const isAuthLayout = computed(() => route.meta.layout === "auth");
     </header>
     <div class="p-4"></div>
 
-    <main class="mx-auto grid max-w-8xl grid-cols-12 gap-4 md:gap-8 px-4 py-2 md:px-0 md:py-0">
-      <div class="col-span-8 space-y-6 md:col-start-2 md:col-span-7 lg:col-start-3 lg:col-span-6">
+    <main
+      class="mx-auto grid max-w-8xl grid-cols-12 gap-4 md:gap-8 px-4 py-2 md:px-0 md:py-0"
+    >
+      <div
+        class="col-span-8 space-y-6 md:col-start-2 md:col-span-7 lg:col-start-3 lg:col-span-6"
+      >
         <RouterView></RouterView>
       </div>
 
-      <div class="col-span-4 space-y-6 md:col-start-9 md:col-span-3 lg:col-span-2">
+      <div
+        class="col-span-4 space-y-6 md:col-start-9 md:col-span-3 lg:col-span-2"
+      >
         <div><UserBalance /></div>
         <div class="p-4"></div>
         <div><Menu /></div>
       </div>
     </main>
-
-    <div class="p-4"></div>
 
     <footer class="mt-16 p-6 text-center text-sm text-gray-500 space-y-1">
       <p>© 2026 Project: Living on n Won. All rights reserved.</p>
